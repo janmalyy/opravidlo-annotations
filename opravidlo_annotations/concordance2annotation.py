05a9bf181a5957b2edfe101a3a2402858268b2be
Jan Malý <x526325@fi.muni.cz>
2025-05-22 16:50:39 +0200

    add extraction of sentence with the target from the concordance

--- opravidlo_annotations/concordance2annotation.py
+++ opravidlo_annotations/concordance2annotation.py
@@ -1,7 +1,62 @@
 import re
 
+import nltk
+
+nltk.download('punkt')
+
+
 def correct_punctuation(text: str) -> str:
     """
-    Corrects punctuation in a string. It removes any trailing whitespaces before the punctuation mark.
+    Corrects punctuation in a string. It removes any trailing whitespaces before or after the punctuation mark.
     """
-    return re.sub(r"\s+([.,?!:;])", r"\1", text)
+    removed_after = re.sub(r"\s+([.,?!:;“])", r"\1", text)
+    removed_before = re.sub(r"(„)\s+", r"\1", removed_after)
+    return removed_before
+
+
+def remove_left_trailing_chars(sentence: str) -> str:
+    """
+    Remove characters before the start of the sentence. This means any characters before the capital letter.
+    If the capital letter is not found or is found after a long distance from the start of the sentence,
+    the sentence is returned unchanged.
+
+    Returns: Sentence without left trailing characters.
+    """
+    pattern = r'^.{0,10}?([A-ZŠČŘŽŠĎŤŇ])'
+    match = re.search(pattern, sentence)
+    if match:
+        start_index = match.start(1)
+        return sentence[start_index:]
+    return sentence
+
+
+def extract_sentence_with_target(concordance: str, target: str) -> str|None:
+    """
+    Extracts a single sentence containing the target word from the given concordance text.
+
+    This function searches the provided concordance text for a sentence that includes
+    the specified target word. A sentence is assumed to end with a period, question mark,
+    or exclamation mark. If the target word is not found within any sentence, the function
+    returns None.
+
+    Args:
+        concordance: A string representing the text containing multiple sentences.
+        target: The word to search for within the concordance text.
+
+    Returns:
+        A string representing the single sentence containing the target word, or None
+        if the target word is not found.
+    """
+    if not concordance or not target:
+        raise ValueError("Empty input. Please provide valid concordance and target.")
+
+    # (?<=...) is a positive lookbehind assertion. It says: “Find a place preceded by the pattern inside the (?<=...), but don’t include it in the result.”
+    # The pattern (?<=[.!?])\s+ matches any whitespace following a sentence-ending punctuation mark but keeps the punctuation as part of the previous sentence.
+    sentences = nltk.sent_tokenize(concordance)
+    for sentence in sentences:
+        pattern = rf"\b{re.escape(target)}\b"
+        match = re.search(pattern, sentence, flags=re.IGNORECASE)
+        if match:
+            return remove_left_trailing_chars(sentence)
+
+    raise ValueError("Target word not found in concordance.")
--- opravidlo_annotations/main.py
+++ opravidlo_annotations/main.py
@@ -1,12 +1,13 @@
 from opravidlo_annotations.kontext import setup_session, fetch_concordances_by_id, submit_query
-from opravidlo_annotations.concordance2annotation import correct_punctuation
+from opravidlo_annotations.concordance2annotation import correct_punctuation, extract_sentence_with_target
 
 if __name__ == "__main__":
     corpus_name = "syn2015"
     # query = "[word=\"yea\"]"  # OK
     # query = "\"yea\""         # OK
     # query = "yea"             # NOK
-    query = "[word=\"mám\"][word=\"velký\"][word=\"hlad\"]"
+    query = "[word=\"modrý\"]"
+    # query = "[word=\"mám\"][word=\"velký\"][word=\"hlad\"]"
     session = setup_session()
     op_id = submit_query(session, corpus_name, query)
     result = fetch_concordances_by_id(session, op_id)
@@ -19,5 +20,5 @@ if __name__ == "__main__":
             lines.append(each["Left"][0]["str"] + each["Kwic"][0]["str"] + each["Right"][0]["str"])
         for line in lines:
             line = correct_punctuation(line)
+            line = extract_sentence_with_target(line, "modrý")
             print(line)
-
