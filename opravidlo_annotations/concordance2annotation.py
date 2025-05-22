6c6a11e6adc81d79cd2515928de26518a6115ea7
Jan Malý <x526325@fi.muni.cz>
2025-05-22 19:06:41 +0200

    add function annotating the sentence

--- opravidlo_annotations/concordance2annotation.py
+++ opravidlo_annotations/concordance2annotation.py
@@ -1,8 +1,9 @@
 import re
+import random as rd
 
 import nltk
 
-nltk.download('punkt')
+nltk.download("punkt")
 
 
 def correct_punctuation(text: str) -> str:
@@ -17,8 +18,10 @@ def correct_punctuation(text: str) -> str:
 def remove_left_trailing_chars(sentence: str) -> str:
     """
     Remove characters before the start of the sentence. This means any characters before the capital letter.
-    If the capital letter is not found or is found after a long distance from the start of the sentence,
-    the sentence is returned unchanged.
+    If the capital letter is not found or is found after a long distance (distance is set to 10)
+    from the start of the sentence, the sentence is returned unchanged. - The stopper is set because if there is
+    no capital letter in the first 10 chars, it probably indicates that it is a long sentence without
+    a start, and we don't want to remove it.
 
     Returns: Sentence without left trailing characters.
     """
@@ -60,3 +63,37 @@ def extract_sentence_with_target(concordance: str, target: str) -> str|None:
             return remove_left_trailing_chars(sentence)
 
     raise ValueError("Target word not found in concordance.")
+
+
+def add_annotation_to_sentence(sentence: str, target:str, target_variants:list[str], is_target_valid:bool, is_from_corpus:bool) -> str:
+    """
+    Insert all information for the annotation into the sentence.
+    If there are multiple variants of the target, one variant is chosen randomly.
+    Args:
+        sentence: sentence to be annotated
+        target: a word or phrase which was mainly looked up in the corpus
+        target_variants: other possible words or phrases which could occur at the same place as target in the sentence
+        is_target_valid: whether the target is orthographically correct or not
+        is_from_corpus: whether the sentence was extracted from the corpus as is or an error was added manually
+
+    Example:
+        input: "Stál před jejích chalupou.", "jejích", ["jejich"], False, False
+        output: Stál před [*jejích|jejich|synthetic*] chalupou.
+
+
+    Returns: Annotated sentence. The resulting format is:
+    beginning_of_the_sentence[*error|valid|(synthetic or corpus)*]rest_of_the_sentence
+    """
+    target_variant = rd.choice(target_variants)
+
+    if is_target_valid:
+        if is_from_corpus:
+            return sentence.replace(target, f"[*{target_variant}|{target}|corpus*]")
+        else:
+            return sentence.replace(target, f"[*{target_variant}|{target}|synthetic*]")
+
+    else:
+        if is_from_corpus:
+            return sentence.replace(target, f"[*{target}|{target_variant}|corpus*]")
+        else:
+            return sentence.replace(target, f"[*{target}|{target_variant}|synthetic*]")
--- opravidlo_annotations/main.py
+++ opravidlo_annotations/main.py
@@ -1,13 +1,12 @@
 from opravidlo_annotations.kontext import setup_session, fetch_concordances_by_id, submit_query
-from opravidlo_annotations.concordance2annotation import correct_punctuation, extract_sentence_with_target
+from opravidlo_annotations.concordance2annotation import correct_punctuation, extract_sentence_with_target, \
+    add_annotation_to_sentence
 
 if __name__ == "__main__":
     corpus_name = "syn2015"
-    # query = "[word=\"yea\"]"  # OK
-    # query = "\"yea\""         # OK
-    # query = "yea"             # NOK
-    query = "[word=\"modrý\"]"
-    # query = "[word=\"mám\"][word=\"velký\"][word=\"hlad\"]"
+    target = "jejich"
+    variants = ["jejích"]
+    query = f"[word=\"{target}\"]"
     session = setup_session()
     op_id = submit_query(session, corpus_name, query)
     result = fetch_concordances_by_id(session, op_id)
@@ -20,5 +19,6 @@ if __name__ == "__main__":
             lines.append(each["Left"][0]["str"] + each["Kwic"][0]["str"] + each["Right"][0]["str"])
         for line in lines:
             line = correct_punctuation(line)
-            line = extract_sentence_with_target(line, "modrý")
+            line = extract_sentence_with_target(line, target)
+            line = add_annotation_to_sentence(line, target, variants, is_target_valid=True, is_from_corpus=True)
             print(line)
