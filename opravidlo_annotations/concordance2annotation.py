3184360a869f62fa130cb3d2964c6d443ab5d08e
Jan Malý <x526325@fi.muni.cz>
2025-05-28 11:44:05 +0200

    improve punctuation correction

--- opravidlo_annotations/concordance2annotation.py
+++ opravidlo_annotations/concordance2annotation.py
@@ -8,11 +8,12 @@ nltk.download("punkt")
 
 def correct_punctuation(text: str) -> str:
     """
-    Corrects punctuation in a string. It removes any trailing whitespaces before or after the punctuation mark.
+    Corrects punctuation in a string. Corrects the three dots and removes any trailing whitespaces before or after the punctuation mark.
     """
-    removed_after = re.sub(r'\s+([.,?!:;“"(¨-])', r"\1", text)
-    removed_before = re.sub(r'([„)"-])\s+', r"\1", removed_after)
-    return removed_before
+    three_dots_corrected = re.sub(r'...', r'…', text)
+    removed_before = re.sub(r'\s+([.,\]?!:;“"…)¨-])', r"\1", three_dots_corrected)
+    removed_after = re.sub(r'([„\[("-])\s+', r"\1", removed_before)
+    return removed_after
 
 
 def remove_left_trailing_chars(sentence: str) -> str:
