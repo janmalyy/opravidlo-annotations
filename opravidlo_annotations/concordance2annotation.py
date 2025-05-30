eacecc32587133f1ff6038dbb14fb5558183ab23
Jan Malý <x526325@fi.muni.cz>
2025-05-30 22:26:59 +0200

    improve punctuation handling: correct hyphen to dash and do not remove opening bracket

--- opravidlo_annotations/concordance2annotation.py
+++ opravidlo_annotations/concordance2annotation.py
@@ -8,11 +8,12 @@ nltk.download("punkt")
 
 def correct_punctuation(text: str) -> str:
     """
-    Corrects punctuation in a string. Corrects the three dots and removes any trailing whitespaces before or after the punctuation mark.
+    Corrects punctuation in a string. Corrects the three dots, hyphen to dash and removes any trailing whitespaces before or after the punctuation mark.
     """
     three_dots_corrected = re.sub(r'\.\.\.', r'…', text)
-    removed_before = re.sub(r'\s+([.,\]?!:;“"…)¨-])', r"\1", three_dots_corrected)
-    removed_after = re.sub(r'([„\[("-])\s+', r"\1", removed_before)
+    dash_corrected = re.sub(r' - ', r' – ', three_dots_corrected)
+    removed_before = re.sub(r'\s+([.,\]?!:;“"…)¨«])', r"\1", dash_corrected)
+    removed_after = re.sub(r'([„\[("»])\s+', r"\1", removed_before)
     return removed_after
 
 
@@ -26,8 +27,8 @@ def remove_left_trailing_chars(sentence: str) -> str:
 
     Returns: Sentence without left trailing characters.
     """
-    # beginning of the sentence, then 0-10 times any other character than „ and then a big letter
-    pattern = r'^[^„]{0,10}?([A-ZŠČŘŽŠĎŤŇ])'
+    # beginning of the sentence, then 0-10 times any other character than „ or ( and then a big letter
+    pattern = r'^[^\(„]{0,10}?([A-ZŠČŘŽŠĎŤŇ])'
     match = re.search(pattern, sentence)
     if match:
         start_index = match.start(1)
