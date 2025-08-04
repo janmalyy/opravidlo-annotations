d016ed75f23d352a969132a24df5cd881eabc36e
Jan Malý <x526325@fi.muni.cz>
2025-08-04 17:52:15 +0200

    main as for shoda podmetu s prisudkem

--- opravidlo_annotations/main.py
+++ opravidlo_annotations/main.py
@@ -7,35 +7,43 @@ from opravidlo_annotations.utils.query_logs import log_the_query, generate_text_
 
 if __name__ == "__main__":
 
-    corpus_manager = "kontext"
-    corpus_name = "syn2020"     # cstenten_all_mj2, cstenten23_mj2
-    koncovka = "i"
+    corpus_manager = "sketch"
+    corpus_name = "cstenten23_mj2"     # cstenten_all_mj2, cstenten23_mj2
+    koncovka = "a"
     target = f"l{koncovka}"
     if koncovka == "i":
-        variants = ["y", "a"]
+        variants = ["y", "a", "o", ""]
     elif koncovka == "a":
-        variants = ["y", "i"]
+        variants = ["y", "i", "o", ""]
     elif koncovka == "y":
-        variants = ["i", "a"]
-    variants_weights = [2/3, 1/3]
+        variants = ["i", "a", "o", ""]
+    variants_weights = [1/4, 1/4, 1/4, 1/4]
     is_target_code = True
 
     # The query is case-sensitive! Remember.
     # Kontext canonical order: 1. slovní druh - 2. určení druhu - 3. rod - 4. číslo - 5. pád - 6. přivl. rod - 7. přivl. číslo - 8. osoba - 9. čas (https://wiki.korpus.cz/doku.php/seznamy:tagy#popis_jednotlivych_pozic_znacky)
     # Sketch engine canonical order: kegncpamdxytzw~ (https://www.sketchengine.eu/tagset-reference-for-czech/)
-    podmet = '[tag="[NPA]..P1...*"]'
-    in_between = '[word!="jsme|jste|by|byste|bychom" & tag!="Z.*"]?'
-    in_between_daleko = '[word!="jsme|jste|by|byste|bychom" & tag!="Vp.*"]'
+    podmet = '[tag="[NPA]..P1.*"]'
+    podmet_sketch = '[tag="k[123].*nP.*c1.*"]'
+    # in_between = '[word!="jsme|jste|by|byste|bychom" & tag!="Z.*"]'
+    in_between = '[tag!="V[Bc].*" & tag!="Z.*"]'
+    in_between_daleko = '[word!="." & tag!="Vp.*" & tag!="N..P1.*" & tag!="V[Bc].*"]'
+    in_between_daleko_sketch = '[word!="." & tag!="k5.*mA.*" & tag!="k1.*c1.*" & word!="jsem|jsi|bych|bys|by"]'
     by = '[word="by"]'
     prisudek = f'[tag="Vp.P....R..A.*" & lemma!="být" & word=".*l{koncovka}"]'
-    not_podmet = '[tag!="[NP]..P1...*"]{3}'
+    prisudek_sketch = f'[tag="k5.*mA.*" & lemma!="být" & word=".*l{koncovka}"]'
+    not_podmet = '[tag!="[NP]..P1.*"]{3}'
+    not_podmet_sketch = '[tag!="k[13].*c1.*"]'
     # query = podmet + in_between + in_between + by + in_between + in_between + prisudek
-    query = podmet + in_between_daleko + '{4,9}' + prisudek + not_podmet
+    # query = not_podmet + prisudek + in_between + '{0,3}' + podmet
+    # query = prisudek + in_between_daleko + '{4,7}' + podmet
+    # query = '[word="jsme"]'
+    query =  not_podmet_sketch + '{5}' + prisudek_sketch + in_between_daleko_sketch + '{0,7}' + podmet_sketch
 
     # 'number_of_concordances_to_fetch' says how many concordances to download. However, it is often more than we want to store as some will be deleted.
     # 'number_of_concordances_to_log' is then the number to appear in the log, it is the final desired number of concordances which will be stored.
-    number_of_concordances_to_fetch = 70
-    number_of_concordances_to_log = 33
+    number_of_concordances_to_fetch = 1500
+    number_of_concordances_to_log = 50
 
     # filename (str): filename to write the annotations into. Only the unique name, the prefix "data_zajmena" and the filename extension will be added.
     filename = f"delal{koncovka}"
